f5d3f45bdbd9e611c1bb534e70e5b443e23962b8
Saughat123 <147581806+Saughat123@users.noreply.github.com>
2023-11-19 19:59:23 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=100;
+int b=200;
+int c=a+b;
+System.out.println("sum="+c);
+int u=2;
+System.out.println(u);
+	}
+
+}
