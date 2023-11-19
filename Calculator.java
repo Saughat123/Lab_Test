5cf3408dc2bcc7722ec1f5697b1c34ccacef444d
Saughat123 <147581806+Saughat123@users.noreply.github.com>
2023-11-19 19:14:44 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,55 +2,61 @@ import java.util.Scanner;
 
 class Calculator{
   public static void main(String[] args) {
+char operator;
+        Double number1, number2, result;
 
-    char operator;
-    Double number1, number2, result;
+        // create an object of Scanner class
+        Scanner input = new Scanner(System.in);
 
-    // create an object of Scanner class
-    Scanner input = new Scanner(System.in);
+        try {
+            // ask users to enter operator
+            System.out.println("Choose an operator: +, -, *, or /");
+            operator = input.next().charAt(0);
 
-    // ask users to enter operator
-    System.out.println("Choose an operator: +, -, *, or /");
-    operator = input.next().charAt(0);
+            // ask users to enter numbers
+            System.out.println("Enter first number");
+            number1 = input.nextDouble();
 
-    // ask users to enter numbers
-    System.out.println("Enter first number");
-    number1 = input.nextDouble();
+            System.out.println("Enter second number");
+            number2 = input.nextDouble();
 
-    System.out.println("Enter second number");
-    number2 = input.nextDouble();
+            switch (operator) {
+                // performs addition between numbers
+                case '+':
+                    result = number1 + number2;
+                    System.out.println(number1 + " + " + number2 + " = " + result);
+                    break;
 
-    switch (operator) {
+                // performs subtraction between numbers
+                case '-':
+                    result = number1 - number2;
+                    System.out.println(number1 + " - " + number2 + " = " + result);
+                    break;
 
-      // performs addition between numbers
-      case '+':
-        result = number1 + number2;
-        System.out.println(number1 + " + " + number2 + " = " + result);
-        break;
+                // performs multiplication between numbers
+                case '*':
+                    result = number1 * number2;
+                    System.out.println(number1 + " * " + number2 + " = " + result);
+                    break;
 
-      // performs subtraction between numbers
-      case '-':
-        result = number1 - number2;
-        System.out.println(number1 + " - " + number2 + " = " + result);
-        break;
+                // performs division between numbers
+                case '/':
+                    if (number2 != 0) {
+                        result = number1 / number2;
+                        System.out.println(number1 + " / " + number2 + " = " + result);
+                    } else {
+                        System.out.println("Error: Division by zero is not allowed.");
+                    }
+                    break;
 
-      // performs multiplication between numbers
-      case '*':
-        result = number1 * number2;
-        System.out.println(number1 + " * " + number2 + " = " + result);
-        break;
-
-      // performs division between numbers
-      case '/':
-        result = number1 / number2;
-        System.out.println(number1 + " / " + number2 + " = " + result);
-        break;
-
-      default:
-        System.out.println("Invalid operator!");
-        break;
+                default:
+                    System.out.println("Invalid operator!");
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input. Please enter valid numbers and operator.");
+        } finally {
+            input.close();
+        }
     }
-
-    input.close();
-  }
 }
